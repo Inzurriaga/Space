529deacdce30f26c6706b2a488278156bfd0a1cc
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-07-23 12:59:54 -0600

    add sun model

--- src/app/App.js
+++ src/app/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import THREE from "three"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="App" ref={mount => {this.mount = mount}}>
+      </div>
+    );
+  }
 }
-
-export default App;
